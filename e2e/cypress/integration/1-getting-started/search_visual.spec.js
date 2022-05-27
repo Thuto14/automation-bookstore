93d34a1debb40580c9d38f078606316b465e39a4
Thuto14 <gregory.tlhabi@gmail.com>
2022-05-27 12:17:28 +0200

    update

--- e2e/cypress/integration/1-getting-started/search_visual.spec.js
+++ e2e/cypress/integration/1-getting-started/search_visual.spec.js
@@ -1,6 +1,6 @@
 describe('Search for books - eyes', () => {
     beforeEach(() => {
-      cy.open('https://abbookstore.azurewebsites.net/')
+      cy.visit('https://abbookstore.azurewebsites.net/')
       cy.eyesOpen()
     })
   
